156ee0e18b01ed852300f69104eb1a10d6b4c89e
Daniel López <daniel.lp1094@gmail.com>
2021-12-23 11:56:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import react, {useState} from "react";
+import { TodoList } from "./components/TodoList";
+
+export function App(){
+    return(
+        <TodoList todos={[
+            {id: 1, task: 'Tarea 1', completed: false},
+        ]}/>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import reactDom from "react-dom";
+import { App } from './App';
+
+//Con React.createElement
+
+/* React.createElement('div', {})//--> Para crear el div sin utilizar jsx y le pasamos un objeto con las propiedades */
+
+//Con reactDom añadiendo div directamente -> ReactDom.render('aqui va el contenido a renderizar' + document.getElementById('root') para embeberlo en el div principal del index.html en public)
+
+/* reactDom.render(<div>Hello World</div>, document.getElementById('root')) */
+
+//Con reactDom añadiendo el componente app (contiene toda nuestra aplicación)
+
+reactDom.render(<App/>, document.getElementById('root'))
