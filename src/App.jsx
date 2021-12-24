220875f6b7e41df7a7bc857393db431973b49e4a
Daniel López <daniel.lp1094@gmail.com>
2021-12-24 11:48:29 +0100

    FEAT: add component TodoListPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import react, { useState } from "react";
-import { TodoList } from "./components/TodoList";
+import React, { Fragment } from 'react'
+import { TodoListPage } from './pages/TodoListPage/TodoListPage'
 
 export function App() {
-
-
-    const [todos, setTodos] = useState([{ id: 1, task: 'Tarea 1', completed: false }]);
-
-
-
     return (
-        <>
-            <TodoList list={todos} />
-            <input type="text" placeholder="Nueva Tarea" />
-            <button>➕</button>
-            <button>🗑️</button>
-        </>
+        <Fragment>
+        <TodoListPage></TodoListPage>
+        </Fragment>
     )
-
-
 }
